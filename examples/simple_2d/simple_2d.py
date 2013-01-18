33a4c750a70fa3f2eb9f2b2ee662d4c6d610ec9d
Adam Ginsburg <keflavich@gmail.com>
2013-01-18 16:59:28 -0700

    fixed example

--- examples/simple_2d/simple_2d.py
+++ examples/simple_2d/simple_2d.py
@@ -15,14 +15,16 @@ import matplotlib as mpl
 from matplotlib import pyplot as pl
 max_npix = max(i.npix for i in d.all_items)
 min_npix = min(i.npix for i in d.all_items)
-color_lambda = lambda value: mpl.cm.jet(((value-min_npix)/float(max_npix-min_npix)))
+color_lambda = lambda item: mpl.cm.jet(((item.npix-min_npix)/float(max_npix-min_npix)))
 
+# for this example, only one line, curves down to the left
 pl.figure(1)
 pl.clf()
 p1=d.plot(style='fluxpix',brightest=True,color=color_lambda)
 p1.axes.set_xscale('log')
 p1.axes.set_yscale('log')
 
+# lots of little branches off the curve made above
 pl.figure(2)
 pl.clf()
 p2=d.plot(style='fluxpix',brightest=False,color=color_lambda)
